d02ff78b06ae721e48bb694e0ea465aa17688da2
notpunchnox <punchnoxpaypal@gmail.com>
2023-12-20 18:04:55 +0100

    correction pages

--- src/controllers/Front/auth.js
+++ src/controllers/Front/auth.js
@@ -4,7 +4,7 @@ module.exports = {
 
     login: (req, res) => {
         try {
-            return res.status(200).sendFile('/public/html/auth.html')
+            return res.status(200).sendFile('src/public/html/auth.html')
         } catch (error) {
             console.error(error)
             return res.status(500).json(Response.error)
@@ -13,7 +13,6 @@ module.exports = {
 
     register: (req, res) => {
         try {
-            console.log(path.resolve('src/public/html/auth.html'))
             return res.status(200).sendFile(path.resolve('src/public/html/auth.html'))
         } catch (error) {
             console.error(error)
--- src/controllers/Front/home.js
+++ src/controllers/Front/home.js
@@ -3,7 +3,7 @@ const Response = require('../../utils/helpers/response.js')
 module.exports = async (req, res) => {
     try {
         
-        return res.status(200).sendFile('/html/home.html')
+        return res.status(200).sendFile('src/public/html/home.html')
 
     } catch (error) {
         console.error(error)
