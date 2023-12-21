cd8b4b9f7297e0b459e7f63a4b0dc845b439d754
notpunchnox <punchnoxpaypal@gmail.com>
2023-12-21 11:55:39 +0100

    ajout du middleware/trajets modals

--- src/controllers/Front/home.js
+++ src/controllers/Front/home.js
@@ -1,9 +1,10 @@
 const Response = require('../../utils/helpers/response.js')
+const path = require('path')
 
 module.exports = async (req, res) => {
     try {
         
-        return res.status(200).sendFile('src/public/html/home.html')
+        return res.status(200).sendFile(path.resolve('src/public/html/Acceuil.html'))
 
     } catch (error) {
         console.error(error)
